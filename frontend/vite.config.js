6a75e925c15955916144fd17503ee88ef489879b
dankim9215 <dankim9215@gmail.com>
2026-01-23 15:18:24 +0900

    real final

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0', // ✨ 외부 접속 허용
+    allowedHosts: true, // ✨ ngrok 같은 외부 도메인 접속 허용 (최신 Vite 필수)
+    port: 5173,
+  }
 })
